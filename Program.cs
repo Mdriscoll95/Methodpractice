4041bece993cb4803f413e049d141a696cc012e5
Mdriscoll95 <michael.driscoll951@gmail.com>
2023-05-12 18:11:27 -0700

    practice

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        MadLibs();
+    }
+    public static void MadLibs()
+    {
+        Console.WriteLine(" what is your name?");
+        var name = Console.ReadLine();
+        Console.WriteLine("favorite color");
+        var color = Console.ReadLine();
+        Console.WriteLine("favorite animal");
+        var animal = Console.ReadLine();
+        Console.WriteLine("favorite band");
+        var band = Console.ReadLine();
+
+        Console.WriteLine($"hello {name}, you like {color}{animal} as a {band}");
+
+
+    }
+
+}
